1ca39c65fca3b0a38da175c4c5f633f69fb87c28
Mojisola Adebiyi <mojisola@softcom.ng>
2020-07-12 22:36:13 +0100

    Add custom http errors

---
+++ BankWithMintAdebiyi/CustomHttpError.swift
@@ -0,0 +1,32 @@
+//
+//  CustomHttpError.swift
+//  BankWithMintAdebiyi
+//
+//  Created by Mojisola Adebiyi on 12/07/2020.
+//  Copyright © 2020 Mojisola Adebiyi. All rights reserved.
+//
+
+import Foundation
+
+
+enum CustomHttpError: String, Error {
+    case NotJson
+    case BadRequest
+    case ServerError
+    case Unknown
+}
+
+extension CustomHttpError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .NotJson:
+            return "Response not in JSON format"
+        case .BadRequest:
+            return "BadRequest"
+        case .ServerError:
+            return "ServerError"
+        case .Unknown:
+            return "Unknown"
+        }
+    }
+}

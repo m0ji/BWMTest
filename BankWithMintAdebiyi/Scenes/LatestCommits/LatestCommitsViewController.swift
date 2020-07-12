17559566295f16acfab2792bba172c529e286411
Mojisola Adebiyi <mojisola@softcom.ng>
2020-07-13 00:41:52 +0100

    Add logic for fetching latest commits

--- BankWithMintAdebiyi/Scenes/LatestCommits/LatestCommitsViewController.swift
+++ BankWithMintAdebiyi/Scenes/LatestCommits/LatestCommitsViewController.swift
@@ -9,30 +9,66 @@
 import UIKit
 
 class LatestCommitsViewController: UIViewController {
-    
     private let cellIdentifier = "\(LatestCommitsTableViewCell.self)"
+    private let githubService = GithubService()
+    private var commits = [GithubCommitResponse]()
+    
+    var refreshControl = UIRefreshControl()
+    
     @IBOutlet private weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
         tableView.register(UINib(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
     }
+    
+    private func setupViews() {
+        navigationItem.title = "Latest Commits"
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(didPullToRefresh), for: .valueChanged)
+        tableView.beginRefreshing()
+    }
+    
+    @objc func didPullToRefresh() {
+        githubService.getLatestCommit(urlParameters: nil){ [weak self] (response) in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.tableView.endRefreshing()
+                self.tableView.reloadData()
+            }
+            switch response {
+            case .success(let success):
+                self.commits = success
+            case .failure(let failure):
+                DispatchQueue.main.async {
+                    self.tableView.showEmptyState(message: "\(failure.localizedDescription)\n\n Pull to refresh")
+                }
+                print(failure)
+            }
+        }
+    }
+
 }
 
 extension LatestCommitsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        if commits.count < 1 {
+            tableView.showEmptyState(message: "No commits in this repo \n Pull to refresh")
+        }else {
+            tableView.hideEmptyState()
+        }
+        return commits.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let commit = commits[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! LatestCommitsTableViewCell
+        cell.configureCell(with: commit)
         return cell
     }
-    
-    
 }
 
 extension LatestCommitsViewController: UITableViewDelegate {
     
 }
-
